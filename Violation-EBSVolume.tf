e3d37eeec5b6584e25f97693152642d1566ee45e
evocke <67954534+evocke@users.noreply.github.com>
2020-07-09 21:14:58 +0200

    Update Violation-EBSVolume.tf
    GitHub commit with true

--- Violation-EBSVolume.tf
+++ Violation-EBSVolume.tf
@@ -10,7 +10,7 @@ resource "aws_ebs_volume" "example" {
   availability_zone = "us-west-2a"
   size              = 40
 
-  encrypted			= false
+  encrypted			= true
 }
 
 resource "aws_instance" "web" {
